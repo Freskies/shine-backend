a0345f1a4d54431bbbfa63704a3e6fc86598bb73
Freskies <43386854+Freskies@users.noreply.github.com>
2025-12-22 11:44:46 +0100

    Refactor member API views and update URL routing

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,5 @@
 from django.urls import path, include
-from . import views
 
 urlpatterns = [
-    path("members", views.get_members),
-    path("members/<int:member_id>", views.get_member),
-    path("members/new", views.create_member),
+    path("members/", include("members.urls")),
 ]
---
+++ members/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.get_members),
+    path("<int:member_id>", views.get_member),
+    path("new", views.create_member),
+]
